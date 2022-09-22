3bf3f96652f5d2c6c8b4e4bcdd89b5c27a91aa97
emrepkcn <64073794+emrepkcn@users.noreply.github.com>
2022-09-22 03:39:10 +0300

    test

--- Elysium.Web/Controllers/HomeController.cs
+++ Elysium.Web/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace Elysium.Web.Controllers
         public IActionResult Index()
         {
             return View();
+            //Test
         }
 
         public IActionResult Privacy()
